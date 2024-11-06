d3b82e0409a7d53c60911ed338da45c1a2c4eba4
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-06 23:21:17 +0600

    updated layout

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -5,7 +5,7 @@ const Root = () => {
 	return (
 		<>
 			<Navbar />
-			<main className="min-h-[calc(100vh-64px)]">
+			<main className="min-h-[calc(100vh-64px)] mt-16 mx-auto px-2 sm:px-6 py-3 md:px-12">
 				<Outlet />
 			</main>
 		</>
