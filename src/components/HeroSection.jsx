153f9d68e061377f0f9233ab95c8ad44abcb21ea
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-17 18:45:13 +0600

    updated hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import facebook from "../assets/fb.gif";
+import facebook from "../assets/facebook.gif";
 import { FaShoppingCart, FaSearch } from "react-icons/fa";
 import { useTypewriter } from "react-simple-typewriter";
 
@@ -9,10 +9,10 @@ export default function HeroSection() {
 	});
 
 	return (
-		<div className="relative bg-gray-900 overflow-hidden my-8">
+		<div className="relative bg-gray-900 overflow-hidden my-8 rounded-lg">
 			<div className="max-w-7xl mx-auto">
 				<div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-					<main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+					<div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
 						<div className="sm:text-center lg:text-left">
 							<h1 className="text-4xl tracking-tight font-extrabold text-gray-100 sm:text-5xl md:text-6xl">
 								<span className="block xl:inline">
@@ -46,7 +46,7 @@ export default function HeroSection() {
 								</div>
 							</div>
 						</div>
-					</main>
+					</div>
 				</div>
 			</div>
 			<div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
